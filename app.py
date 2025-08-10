b1715506b709d49fcf58cbe450254cb62c4df57d
ArufaNguyen <lamnguyetda1234@gmail.com>
2025-08-10 20:48:06 +0700

    a

--- app.py
+++ app.py
@@ -144,6 +144,24 @@ def api_dashboard_data():
     # cursor.close()
     # conn.close()
     return results
+@app.route('/api/users')
+def api_users_data():
+    conn = pyodbc.connect(
+        'DRIVER={SQL Server};'
+        'SERVER=localhost,1433;'
+        'DATABASE=QuanLyXeDB;'
+        'UID=sa;'
+        'PWD=Aa123456'
+    )
+    cursor = conn.cursor()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    columns = [column[0] for column in cursor.description]
+    # Convert từng Row thành dict
+    results = [dict(zip(columns, row)) for row in cursor.fetchall()]
+    # cursor.close()
+    # conn.close()
+    return results
 
 @app.route('/payment-success')
 def payment_success():
@@ -172,6 +190,59 @@ def checkin():
         "message": f"Xe {ID_xe} đã check-in",
         "plate_image": plate_image_url
     })    
+@app.route('/dashboard/account')
+def data_get():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    username = session['username']
+    role = session['role']
+    money = to_money(username)
+    # Lấy các biến bạn cần truyền
+    dashboard_data = DashboardDataRepository().load_dashboard_data() 
+    so_xe_vao = dashboard_data.get('xe_vao')
+    so_xe_ra = dashboard_data.get('xe_ra')
+    slot_trong = dashboard_data.get('slot_trong')
+    doanh_thu = dashboard_data.get('tien_vao_hom_nay')
+    most_use_slot = dashboard_data.get('most_use_slot')
+    least_use_slot = dashboard_data.get('least_use_slot')
+    admin_online = dashboard_data.get('admin_online')
+    # Bạn có thể load các dữ liệu khác phục vụ dashboard tại đây
+    dashboard_data = AccountRepository().load_username_data()
+    for i in dashboard_data:
+        if i.get("username") == username:
+            ID_xe = i.get('ID_xe')
+            qr_code = i.get('qr_code')
+            Time_used = i.get('Time_used')
+            Time_started = i.get('Time_started')
+            Day_started = i.get('Day_started')
+            Day_Pass = i.get('Day_Pass')
+            hours= to_time(username)["hours"]
+            minutes = to_time(username)["minutes"]
+    if role == 'admin':
+        return render_template(
+            'Account.html',
+            username=username,
+            money=money,
+            so_xe_vao=so_xe_vao,
+            so_xe_ra=so_xe_ra,
+            slot_trong=slot_trong,
+            doanh_thu=doanh_thu,
+            most_use_slot=most_use_slot,
+            least_use_slot=least_use_slot,
+            admin_online=admin_online,
+        )    
+    else:
+        return render_template(
+            'dashboard.html', 
+            username=username, 
+            money=money, 
+            Time_started=Time_started,
+            ID_xe=ID_xe,
+            qr_code=qr_code,
+            hours=int(hours),
+            minutes=int(minutes)
+        )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
